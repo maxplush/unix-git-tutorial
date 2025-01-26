9db9075503568d8bb90b4f47c4bc463f781c1b4e
maxplush <maxabeplush@gmail.com>
2025-01-26 14:35:53 -0800

    added user input

--- message.py
+++ message.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 
-def main(message):
-    print(f'{message} world!')
+def main(message,input_name):
+    if input_name:
+        print('enter your name')
+        name = input()
+    else:
+        name = 'world'
+    print(f'{message} {name}!')
 
 if __name__=='__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--message',choices=['hello','goodbye'])
+    parser.add_argument('--input_name',action='store_true')
     args = parser.parse_args()
-    main(args.message)
+    main(args.message,args.input_name)
