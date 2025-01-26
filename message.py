aae0261134cc4abb467690d1b2f66ad2d8ee3bf1
maxplush <maxabeplush@gmail.com>
2025-01-26 14:39:15 -0800

    fixed the message bug

--- message.py
+++ message.py
@@ -6,6 +6,6 @@ def main(message):
 if __name__=='__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--message',choices=['hello','goodbye'])
+    parser.add_argument('--message',choices=['hello','goodbye'],default='hello')
     args = parser.parse_args()
     main(args.message)
